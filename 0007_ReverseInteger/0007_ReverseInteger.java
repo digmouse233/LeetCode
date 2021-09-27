d8c1400c99894732f5e4331faadd5457dcf135be
digmouse233 <digmouse233@gmail.com>
2021-09-27 08:06:17 -0400

    Add Reverse Integer Solution in Java

---
+++ 0007_ReverseInteger/0007_ReverseInteger.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int reverse(int x) {
+        int res = 0;
+
+        while (x != 0) {
+            int pop = x % 10;
+
+            if (res > Integer.MAX_VALUE / 10 || 
+                (res == Integer.MAX_VALUE / 10 && pop >= Integer.MAX_VALUE % 10)) {
+                return 0;
+            }
+
+            if (res < Integer.MIN_VALUE / 10 ||
+                (res == Integer.MIN_VALUE / 10 && pop <= Integer.MIN_VALUE % 10)) {
+                return 0;
+            }
+
+            res = res * 10 + pop;
+            x /= 10;
+        }
+
+        return res;
+    }
+}
+
+
+
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+}

0315ff1617c71716b10addce8424e927bba5ba3e
digmouse233 <digmouse233@gmail.com>
2021-10-14 22:48:31 -0400

    Add Two Sum Solution in Java

--- 0001_TwoSum/Solution.java
+++ 0001_TwoSum/Solution.java
@@ -2,9 +2,11 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         var res = new int[2];
         var map = new HashMap(); 
+
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], i);
         }
+
         for (int i = 0; i < nums.length; i++) {
             int gap = target - nums[i];
             if (map.get(gap) != null && (int) map.get(gap) != i) {
@@ -12,6 +14,7 @@ class Solution {
                 res[1] = (int) map.get(gap);
             }
         }
+        
         return res;
     }
 }
